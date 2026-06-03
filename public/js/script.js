04780bb03a5fea8b96d9cd3502198411d4229a1c
sharma divyanshi <sharmadivyanshi834@gmail.com>
2026-06-03 16:44:44 +0530

    Added live location feature

---
+++ public/js/script.js
@@ -0,0 +1,49 @@
+const socket = io();
+
+
+if(navigator.geolocation){
+    navigator.geolocation.watchPosition((position) =>{
+        const {latitude, longitude} = position.coords;
+        socket.emit("send-location", {latitude, longitude});
+    }, (err)=>{
+        console.err(err)
+    },{
+        enableHighAccuracy:true,
+        timeout:5000,
+        maximumAge:0
+    });
+};
+
+
+
+const map = L.map("map").setView([0,0], 16);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:"OpenStreetMap"
+}).addTo(map)
+
+
+
+const marker = {}
+
+
+
+socket.on("receive-location", (data)=>{
+     console.log(data.id);
+    const{id, latitude, longitude} = data;
+
+    map.setView([latitude, longitude])
+    if(marker[id]){
+        marker[id].setLatLng([latitude, longitude])
+    }else{
+        marker[id] = L.marker([latitude, longitude]).addTo(map)
+    }
+})
+
+
+socket.on('user-disconnected', (id)=>{
+    if(marker[id]){
+        map.removeLayer(marker[id]);
+        delete marker[id];
+    }
+});
